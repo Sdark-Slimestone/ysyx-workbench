0ada11e263b537406fcce5ba0dc319ef249330ff
0-Tan ZiJian <3047808825@qq.com>
2026-04-07 18:47:15 +0800

    NJU-ProjectN/abstract-machine ics2024 initialized
    NJU-ProjectN/abstract-machine ad41f4948e185243ca07f6d2a9ef296f7248d617 tools,minirv,minirv-common.sh: set flag for minirv in ELF

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,138 +5,43 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  size_t i = 0;
-  for(const char *p = s; *p != '\0'; p++){
-    i++;
-  }
-  return i;
+  panic("Not implemented");
 }
 
 char *strcpy(char *dst, const char *src) {
-  char *dst_pre = dst;
-  size_t size = strlen(src);
-  for(size_t i = 0; i <= size; i++){
-    *dst = *src;
-    dst++;
-    src++;
-  }
-  return dst_pre;
+  panic("Not implemented");
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  char *dst_pre = dst;
-  size_t size = strlen(src);
-  if(size < n){
-    for(size_t i = 1; i <= n; i++){
-      if(i <= size){
-        *dst = *src;
-        dst++;
-        src++;
-      }
-      else{
-        *dst = '\0';
-        dst++;
-      }
-    }
-    return dst_pre;
-  }
-  else {
-    for(size_t i = 1; i <= n; i++){
-      *dst = *src;
-      dst++;
-      src++;
-    }
-    return dst_pre;
-  }
+  panic("Not implemented");
 }
 
 char *strcat(char *dst, const char *src) {
-  char *dst_pre = dst;
-  size_t size = strlen(dst);
-  strcpy((dst+size), src);
-  return dst_pre;
+  panic("Not implemented");
 }
 
 int strcmp(const char *s1, const char *s2) {
-  int t;
-  while(*s1 == *s2 && *s1 !='\0'){
-    s1++;
-    s2++;
-  }
-  t = (*(unsigned char*)s1 -*(unsigned char*)s2);
-  return t;
+  panic("Not implemented");
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  int t;
-  for(size_t i = 1; i <= n; i++){
-    if(*s1 == *s2 && *s1 !='\0'){
-      s1++;
-      s2++;
-    }
-    else{
-      t = (*(unsigned char*)s1 -*(unsigned char*)s2);
-      return t;
-    }
-  }
-  return 0;
+  panic("Not implemented");
 }
 
 void *memset(void *s, int c, size_t n) {
-  unsigned char *p = (unsigned char *)s;
-  for(size_t i = 1; i <= n; i++){
-    *p = (unsigned char)c;
-    p++;
-  }
-  return s;
+  panic("Not implemented");
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  unsigned char *d = (unsigned char*)dst;
-    const unsigned char *s = (const unsigned char*)src;
-    if (d < s) {
-        // 正向拷贝：从前往后
-        for (size_t i = 1; i <= n; i++) {
-            *d = *s;
-            d++;
-            s++;
-        }
-    } else if (d > s) {
-        // 反向拷贝：从后往前
-        d += n - 1;
-        s += n - 1;
-        for (size_t i = 1; i <= n; i++) {
-            *d = *s;
-            d--;
-            s--;
-        }
-    }
-    // 若 d = s，什么都不做
-    return dst;
+  panic("Not implemented");
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  unsigned char *o = (unsigned char *)out;
-    const unsigned char *i = (const unsigned char *)in;
-    for (size_t cnt = 1; cnt <= n; cnt++) {
-        *o = *i;
-        o++;
-        i++;
-    }
-    return out;
+  panic("Not implemented");
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  const unsigned char *p1 = (const unsigned char *)s1;
-    const unsigned char *p2 = (const unsigned char *)s2;
-    for (size_t i = 1; i <= n; i++) {
-        if (*p1 != *p2) {
-            return (int)(*p1 - *p2);
-        }
-        p1++;
-        p2++;
-    }
-    return 0;
+  panic("Not implemented");
 }
 
 #endif
