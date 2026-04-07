835417aacc03cfd63d8b8cd7445de3dfada9bfd3
0-Tan ZiJian <3047808825@qq.com>
2026-04-07 19:55:29 +0800

    完成d4

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,43 +5,138 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t i = 0;
+  for(const char *p = s; *p != '\0'; p++){
+    i++;
+  }
+  return i;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  char *dst_pre = dst;
+  size_t size = strlen(src);
+  for(size_t i = 0; i <= size; i++){
+    *dst = *src;
+    dst++;
+    src++;
+  }
+  return dst_pre;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  char *dst_pre = dst;
+  size_t size = strlen(src);
+  if(size < n){
+    for(size_t i = 1; i <= n; i++){
+      if(i <= size){
+        *dst = *src;
+        dst++;
+        src++;
+      }
+      else{
+        *dst = '\0';
+        dst++;
+      }
+    }
+    return dst_pre;
+  }
+  else {
+    for(size_t i = 1; i <= n; i++){
+      *dst = *src;
+      dst++;
+      src++;
+    }
+    return dst_pre;
+  }
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  char *dst_pre = dst;
+  size_t size = strlen(dst);
+  strcpy((dst+size), src);
+  return dst_pre;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  int t;
+  while(*s1 == *s2 && *s1 !='\0'){
+    s1++;
+    s2++;
+  }
+  t = (*(unsigned char*)s1 -*(unsigned char*)s2);
+  return t;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  int t;
+  for(size_t i = 1; i <= n; i++){
+    if(*s1 == *s2 && *s1 !='\0'){
+      s1++;
+      s2++;
+    }
+    else{
+      t = (*(unsigned char*)s1 -*(unsigned char*)s2);
+      return t;
+    }
+  }
+  return 0;
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  unsigned char *p = (unsigned char *)s;
+  for(size_t i = 1; i <= n; i++){
+    *p = (unsigned char)c;
+    p++;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  unsigned char *d = (unsigned char*)dst;
+    const unsigned char *s = (const unsigned char*)src;
+    if (d < s) {
+        // 正向拷贝：从前往后
+        for (size_t i = 1; i <= n; i++) {
+            *d = *s;
+            d++;
+            s++;
+        }
+    } else if (d > s) {
+        // 反向拷贝：从后往前
+        d += n - 1;
+        s += n - 1;
+        for (size_t i = 1; i <= n; i++) {
+            *d = *s;
+            d--;
+            s--;
+        }
+    }
+    // 若 d = s，什么都不做
+    return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  unsigned char *o = (unsigned char *)out;
+    const unsigned char *i = (const unsigned char *)in;
+    for (size_t cnt = 1; cnt <= n; cnt++) {
+        *o = *i;
+        o++;
+        i++;
+    }
+    return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  const unsigned char *p1 = (const unsigned char *)s1;
+    const unsigned char *p2 = (const unsigned char *)s2;
+    for (size_t i = 1; i <= n; i++) {
+        if (*p1 != *p2) {
+            return (int)(*p1 - *p2);
+        }
+        p1++;
+        p2++;
+    }
+    return 0;
 }
 
 #endif
--- abstract-machine/scripts/minirv-npc.mk
+++ abstract-machine/scripts/minirv-npc.mk
@@ -2,15 +2,18 @@ include $(AM_HOME)/scripts/isa/riscv.mk
 include $(AM_HOME)/scripts/platform/npc.mk
 
 export PATH := $(PATH):$(abspath $(AM_HOME)/tools/minirv)
-CC = minirv-gcc
-AS = minirv-gcc
+CC  = minirv-gcc
+AS  = minirv-gcc
 CXX = minirv-g++
 
-COMMON_CFLAGS += -march=rv32e_zicsr -mabi=ilp32e  # overwrite
-LDFLAGS       += -melf32lriscv                    # overwrite
+COMMON_CFLAGS += -march=rv32e_zicsr -mabi=ilp32e
+LDFLAGS       += -melf32lriscv
 
 AM_SRCS += riscv/npc/libgcc/div.S \
            riscv/npc/libgcc/muldi3.S \
            riscv/npc/libgcc/multi3.c \
            riscv/npc/libgcc/ashldi3.c \
            riscv/npc/libgcc/unused.c
+
+run: $(IMAGE).bin
+	/home/sdark/cpu_project/MyChisel/verilog/obj_dir/npc $<
